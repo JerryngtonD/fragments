d3f3d921f2079413f75f340dd7ec70a66c593a3b
jerryngton <jerryngtond@gmail.com>
2020-04-30 09:40:47 +0300

    run fragment from activity via support fragment manager

--- app/src/main/java/ru/otus/fragments/NewsListFragment.kt
+++ app/src/main/java/ru/otus/fragments/NewsListFragment.kt
@@ -1,6 +1,8 @@
 package ru.otus.fragments
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,12 +12,40 @@ import ru.otus.fragments.recyclerTwo.NewsAdapter
 import ru.otus.fragments.recyclerTwo.NewsItem
 
 class NewsListFragment : Fragment() {
+    companion object {
+        const val TAG = "NewsListFragment"
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate $this")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy  $this")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d(TAG, "onCreateView")
         return inflater.inflate(R.layout.fragment_news_list, container, false)
+
+    }
+
+    override fun onDestroyView() {
+        Log.d(TAG, "onDestroyView")
+        super.onDestroyView()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d(TAG, "onViewCreated")
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<RecyclerView>(R.id.recyclerView).adapter = NewsAdapter(LayoutInflater.from(activity), listOf(
             NewsItem(0, "News 0"),
